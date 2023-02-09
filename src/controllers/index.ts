47cc4272310602388ddf2860b5afdeb9814e0c80
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-09 23:04:03 +0300

    updated code

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,43 +1,43 @@
 import { RequestHandler,Request,Response } from 'express'
 import {v4 as uid} from 'uuid'
 // import { bookingSchema } from '../Helpers'
-import { Booking } from '../models'
-// import { DatabaseHelper } from '../DatabaseHelpers'
-// const _db= new DatabaseHelper()
+import { Todos } from '../models'
+import { DatabaseHelper } from '../databaseHelpers'
+const _db= new DatabaseHelper()
 interface ExtendedRequest extends Request{
-    body:{Name:string,Email:string,Destination:string, TravelDate:string},
-    params:{id:string}
+    body:{title:string,description:string,date:string},
+    params:{taskId:string}
 }
 //Get booking Details
-export const getBookings:RequestHandler=async (req,res)=>{
+export const getTodos:RequestHandler=async (req,res)=>{
    try {
-      const bookings:Booking[] = await (await _db.exec('getFlights')).recordset
-     res.status(200).json(bookings)
+      const todo:Todos[] = await (await _db.exec('getTodos')).recordset
+     res.status(200).json(todo)
    } catch (error) {
     res.status(500).json(error)
    }
 
 }
 //Get one booking
-export const getOneBooking=async(req:ExtendedRequest,res:Response)=>{
+export const getOneTodo=async(req:ExtendedRequest,res:Response)=>{
 try {
-  const id = req.params.id
-  const booking:Booking= await (await  _db.exec('getFlightBookings', {id})).recordset[0]
-  if(!booking){
-     res.status(404).json({error:'Booking Not Found'})
+  const taskId = req.params.taskId
+  const todo:Todos= await (await  _db.exec('getOneTodo', {taskId})).recordset[0]
+  if(!todo){
+     res.status(404).json({error:'Todo Not Found'})
   }
 
-  res.status(200).json(booking)
+  res.status(200).json(todo)
 
 } catch (error) {
   res.status(500).json(error)
 }
 
 }
- export async function addBooking( req:ExtendedRequest, res:Response) {
+ export async function addTodo( req:ExtendedRequest, res:Response) {
   try {
-    const id =uid()
-    const {Name,Email,TravelDate,Destination}= req.body
+    const taskId =uid()
+    const {title,description,date}= req.body
     
     console.log(req.body);
     
@@ -45,9 +45,9 @@ try {
     //   return res.status(422).json(error.details[0].message)
     // }
     _db.exec('InsertOrUpdate', 
-    {id,name:Name, email:Email, destination:Destination,date:TravelDate})
+    {taskId,title:title, description:description, date:date})
 
-   return  res.status(201).json({message:'Booking Added'})
+   return  res.status(201).json({message:'Todo Added'})
   } 
   catch (error:any) {
      return res.status(500).json(error.message)
@@ -60,15 +60,15 @@ try {
 
 export async function updateBooking(req:ExtendedRequest,res:Response){
 try {
-const {Name,Email,TravelDate,Destination}= req.body
-     const booking:Booking= await (await _db.exec('getFlightBookings',{id:req.params.id})).recordset[0]
+  const {title,description,date}= req.body
+     const todo:Todos= await (await _db.exec('getOneTodo',{taskId:req.params.taskId})).recordset[0]
 
-  if(booking){
-    await _db.exec('InsertOrUpdate', {id:req.params.id,name:Name, email:Email, destination:Destination, date:TravelDate})
+  if(todo){
+    await _db.exec('InsertOrUpdate', {taskIdd:req.params.taskId,title:title, description:description, date:date})
     return res.status(200).json({message:'Updated'})
   }
 
-  return res.status(404).json({error:'Booking Not Found'})    
+  return res.status(404).json({error:'Todo Not Found'})    
   } 
 
 catch (error:any) {
@@ -80,16 +80,16 @@ catch (error:any) {
 //delete
 
 
-export const cancelBooking=async(req:ExtendedRequest, res:Response)=>{
+export const deleteTodo=async(req:ExtendedRequest, res:Response)=>{
   try {
-    const booking:Booking= await (
-    await _db.exec('getFlightBookings',{id:req.params.id})
+    const todo:Todos= await (
+    await _db.exec('getOneTodo',{taskId:req.params.taskId})
   ).recordset[0]
-    if(booking){
-          await _db.exec('deleteFlightBookings',{id:req.params.id})
+    if(todo){
+        await _db.exec('deleteTodo',{taskId:req.params.taskId})
         return res.status(200).json({message:'Deleted'})
     }
-  return res.status(404).json({error:'Booking Not Found'})  
+  return res.status(404).json({error:'Todo Not Found'})  
   } catch (error:any) {
     res.status(500).json(error.message)
   }
