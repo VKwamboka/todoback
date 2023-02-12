d176865a9eae89571ae9f01e4b9401a04100b126
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-12 22:57:11 +0300

    updated code

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,6 @@
 import { RequestHandler,Request,Response } from 'express'
 import {v4 as uid} from 'uuid'
-import { LoginSchema, RegistrationSchema } from '../Helpers'
+import { LoginSchema, RegistrationSchema } from '../helpers'
 import { DecodedData, User } from '../models'
 import Bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
