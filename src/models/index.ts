0b59a055991a08a2d38145b861c57e4000d32d52
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-12 22:28:30 +0300

    updated code

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,18 @@ export class Todos{
     constructor(public TaskId:string, public Title:string , 
         public Description:string, public taskDate:string){}
 }
+
+export class User{
+    constructor(public Id:string, public Name:string , 
+        public Email:string, public Password:string, public Role:string){}
+}
+
+
+export interface DecodedData{
+  Id: string,
+  Name:string,
+  Email:string,
+  Role: string,
+  iat: number
+  exp: number
+}
